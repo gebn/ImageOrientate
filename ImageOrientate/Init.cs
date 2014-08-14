801e8dd4a30b90dbf2a1968f4bed0626ca7f99c6
George Brighton <george@gebn.co.uk>
2014-08-14 16:49:51 +0100

    Added project files and an Init class with Main() method so ImageOrientate works from the command line.

---
+++ ImageOrientate/Init.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+
+namespace ImageOrientate
+{
+	/// <summary>
+	/// Main class for running ImageOrientate from the CLI.
+	/// </summary>
+	static class Init
+	{
+		public static void Main(string[] args)
+		{
+			foreach (string path in args)
+			{
+				try
+				{
+					using (Image img = Image.FromFile(path))
+					{
+						// rotate the image, if necessary
+						if (ImageOrientate.Process(img))
+						{
+							// save the modified image back to its original path
+							using (FileStream file = new FileStream(path, FileMode.Create))
+							{
+								img.Save(file, ImageFormat.Jpeg);
+							}
+						}
+					}
+				}
+				catch (FileNotFoundException)
+				{
+					Console.Error.WriteLine(String.Format("Couldn't read the image at {0}.", path));
+				}
+				catch (Exception e)
+				{
+					Console.Error.WriteLine(String.Format("Unable to process '{0}': {1}.", path, e.Message));
+				}
+			}
+		}
+	}
+}
